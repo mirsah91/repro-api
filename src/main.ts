6cbdc6fdf538db583e90fda3cd8f0d07c67edbf3
Mihran Sahakyan <mihran.sahakyan@korioclinical.com>
2025-09-16 05:47:50 +0400

    handle big chunks

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as express from 'express'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-
+  app.use(express.json({ limit: '20mb' })); // requests
+  app.use(express.urlencoded({ extended: true, limit: '20mb' }));
   const config = new DocumentBuilder()
       .setTitle('Repro API')
       .setVersion('0.1.0')
